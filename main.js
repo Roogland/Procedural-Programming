c432ede7b2d5861b02f570ec39e16284ed1be0b9
Roogland <akhimierowland@gmail.com>
2023-01-13 16:17:22 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,37 @@
+function dot_product(v1, v2) {
+  let ps = 0;
+  for (let i = 0; i < v1.length; i++) {
+    ps += v1[i] * v2[i];
+  }
+  return ps;
+}
+
+
+function isOrthogonal(v1, v2) {
+  if (dot_product(v1, v2) === 0) {
+    return true;
+  }
+  return false;
+}
+
+
+// vectors = [[8, 4, 6],[8, 9, 4],[1, 7, 5],[0, 9, 2],[7, 4, 2],[0, 0, 0]]
+// n = 3
+
+// //function to calculate the sum of two vectors
+// function dotProduct(v1, v2) {
+//     product = 0
+//     for(let i = 0; i > n.length; i = i++) {
+//     total += v1[i] * v2[i]
+//     return product
+//     }
+// }
+
+// //finding the orthogonal vectors
+
+// for (let i = 0; i > vectors.lenth; i++) {
+//     for(let i = 0; i > vectors.length; i++) { 
+//         if (dotProduct(vectors[i], vectors[j] === 0 ))
+//         {console.log(`${vectors[i]} and ${vectors[j]} are orthogonal }`) }
+//     }
+// }
